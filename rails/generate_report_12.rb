921c0fa20cc50e6837f599d5a1fa3ca94bea3073
Omar Rodriguez Arenas <orodrigu@ualberta.ca>
2022-06-02 10:20:08 -0600

    WIP Report 12

--- rails/generate_report_12.rb
+++ rails/generate_report_12.rb
@@ -1,18 +1,34 @@
+# Report 12
+root_directory = "./era_audit/"
+
 # Check if meber of path has correct information
 missing_community_collection = false
 missing_community_collection_report = {community: [], collection: []}
 
-entity.member_of_paths.each do |pair|
-  
-  community_collection = pair.split('/')
-  unless Community.find community_collection[0]
-    missing_community_collection_report[:community] << "Community Id %{community_id} does not exist" % {community_id: community_collection[0]}
-    missing_community_collection = true
-  end
+[Item, Thesis].each do |klass|
+  entity_type = klass.name.underscore
+  entity_attributes = klass.first.attributes.keys
+  entity_headers = entity_attributes.map {|key| klass.rdf_annotation_for_attr(key).present? ? RDF::URI(klass.rdf_annotation_for_attr(key).first.predicate).pname.to_s : key }
+  file_name = root_directory + '/report_12_' + entity_type + '_community_collection_pairs_that_dont_exist' + Time.now.to_formatted_s(:number) + '.csv'
+  CSV.open(file_name, 'wb', write_headers: true, headers: entity_headers + ['Community errors', 'Collection errors']) do |csv|
+    klass.find_each do |entity|
+      entity.member_of_paths.each do |pair|
+        missing_community_collection = false
+        community_collection = pair.split('/')
+        unless Community.find community_collection[0]
+          missing_community_collection_report[:community] << "Community Id %{community_id} does not exist" % {community_id: community_collection[0]}
+          missing_community_collection = true
+        end
 
-  unless Collection.find community_collection[1]
-    missing_community_collection_report[:collection] << "Collection Id %{collection_id} does not exist" % {collection_id: community_collection[1]}
-    missing_community_collection = true
-  end
+        unless Collection.find community_collection[1]
+          missing_community_collection_report[:collection] << "Collection Id %{collection_id} does not exist" % {collection_id: community_collection[1]}
+          missing_community_collection = true
+        end
 
+        if missing_community_collection
+          csv << entity.values_at(entity_attributes) + [missing_community_collection_report[:community].to_json, missing_community_collection_report[:collection].to_json]
+        end
+      end
+    end
+  end
 end
