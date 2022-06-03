3096f709470bfadb5790f00bfed69a209d23c5b7
Omar Rodriguez Arenas <orodrigu@ualberta.ca>
2022-06-03 12:28:02 -0600

    Add report 14 finding communities with no logo

---
+++ rails/generate_report_14.rb
@@ -0,0 +1,18 @@
+# Report 14
+root_directory = "./era_audit/"
+
+
+def get_community_url(community)
+  # URL example: https://era.library.ualberta.ca/communities/d1640714-da95-4963-9242-68065fece5f4
+  "https://era.library.ualberta.ca/communities/%{id}" % {id: community.id}
+end
+
+file_name = root_directory + '/report_14_' + 'communities_with_no_logo_' + Time.now.to_formatted_s(:number) + '.csv'
+
+CSV.open(file_name, 'wb', write_headers: true, headers: ['Community title', 'Community URL']) do |csv|
+  Community.find_each do |community|
+    unless community.logo.attached?
+      csv << [community.title, get_community_url(community)]
+    end
+  end
+end
