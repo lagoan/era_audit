97a85cf9c7dcf4de511c21283908a22b8d6f0894
Omar Rodriguez Arenas <orodrigu@ualberta.ca>
2022-06-07 10:41:57 -0600

    Fix report 12
    This fix will also check if the pair exists to avoid problems where both
    Community and Collection exist but they are not related from the value
    saved in the member_of_paths pair.

--- rails/generate_report_12.rb
+++ rails/generate_report_12.rb
@@ -1,39 +1,50 @@
 # Report 12
 root_directory = "./era_audit/"
 
-# Check if meber of path has correct information
-missing_community_collection = false
-missing_community_collection_report = {community: [], collection: []}
-
 [Item, Thesis].each do |klass|
   entity_type = klass.name.underscore
   entity_attributes = klass.first.attributes.keys
   entity_headers = entity_attributes.map {|key| klass.rdf_annotation_for_attr(key).present? ? RDF::URI(klass.rdf_annotation_for_attr(key).first.predicate).pname.to_s : key }
   file_name = root_directory + '/report_12_' + entity_type + '_community_collection_pairs_that_dont_exist_' + Time.now.to_formatted_s(:number) + '.csv'
-  CSV.open(file_name, 'wb', write_headers: true, headers: entity_headers + ['Community errors', 'Collection errors']) do |csv|
+  CSV.open(file_name, 'wb', write_headers: true, headers: entity_headers + ['Community errors', 'Collection errors', 'Pair errors']) do |csv|
     klass.find_each do |entity|
+
+      # Check if meber of path has correct information
+      missing_community_collection = false
+      missing_community_collection_report = {community: [], collection: [], pair: []}
+
       entity.member_of_paths.each do |pair|
         missing_community_collection = false
         # When splitting the member of paths pair, the first id is for community
         # and second one is for collection
         community_collection = pair.split('/')
 
+        community = nil
+        collection = nil
+
         begin 
-          Community.find community_collection[0]
+          community = Community.find community_collection[0]
         rescue ActiveRecord::RecordNotFound
           missing_community_collection_report[:community] << "Community Id %{community_id} does not exist" % {community_id: community_collection[0]}
           missing_community_collection = true
         end
 
         begin 
-          Collection.find community_collection[1]
+          collection = Collection.find community_collection[1]
         rescue ActiveRecord::RecordNotFound
           missing_community_collection_report[:collection] << "Collection Id %{collection_id} does not exist" % {collection_id: community_collection[1]}
           missing_community_collection = true
         end
 
+        if community && collection
+          unless collection.community == community
+            missing_community_collection_report[:pair] << "Pair %{pair} does not exist" % {pair: pair}
+            missing_community_collection = true
+          end
+        end
+
         if missing_community_collection
-          csv << entity.values_at(entity_attributes) + [missing_community_collection_report[:community].to_json, missing_community_collection_report[:collection].to_json]
+          csv << entity.values_at(entity_attributes) + [missing_community_collection_report[:community].join(', '), missing_community_collection_report[:collection].join(', '), missing_community_collection_report[:pair].join(', ')]
         end
       end
     end
