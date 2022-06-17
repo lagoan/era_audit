c79247dc0bb112df77f6aef19812b42081cf7e71
Omar Rodriguez Arenas <orodrigu@ualberta.ca>
2022-06-17 12:37:58 -0600

    Improve report 7
    Add more information about community and collection to make report more
    useful.

--- rails/generate_report_7.rb
+++ rails/generate_report_7.rb
@@ -2,18 +2,33 @@
 
 root_directory = './era_audit/'
 
-def get_collection_url(collection)
-  # URL example: https://era.library.ualberta.ca/communities/34de6895-e488-440b-b05c-75efe26c4971/collections/67e0ecb3-05b7-4c9a-bf82-31611e2dc0ce
-  format('https://era.library.ualberta.ca/communities/%{community_id}/collections/%{collection_id}',
-         community_id: collection.community.id, collection_id: collection.id)
+
+community_collection_headers = [
+  'Community title',
+  'Community URL',
+  'Collection title',
+  'Collection URL',
+  'Community Collection pair'
+]
+
+def get_community_collection_values(collection)
+  community = collection.community
+
+  [
+    community.title,
+    Rails.application.routes.url_helpers.community_url(community.id),
+    collection.title,
+    Rails.application.routes.url_helpers.community_collection_url(community.id, collection.id),
+    collection.path
+  ]
 end
 
 CSV.open(
-  root_directory + '/report_7_community_collection_pair_no_entities_' + Time.now.to_formatted_s(:number) + '.csv', 'wb', write_headers: true, headers: [
-    'Community Collection pair', 'URL'
-  ]
+  root_directory + '/report_7_community_collection_pair_no_entities_' + Time.now.to_formatted_s(:number) + '.csv', 'wb',
+  write_headers: true,
+  headers: community_collection_headers
 ) do |csv|
   Collection.find_each do |collection|
-    csv << [collection.path, get_collection_url(collection)] if collection.member_objects.size.zero?
+    csv << get_community_collection_values(collection) if collection.member_objects.size.zero?
   end
 end
