8ab66c5d75e7f5a293998d17004ccce76c2a0592
Omar Rodriguez Arenas <orodrigu@ualberta.ca>
2022-06-17 12:37:22 -0600

    Improve report 6
    Add more information about the community and collection to make report
    more useful.

--- rails/generate_report_6.rb
+++ rails/generate_report_6.rb
@@ -9,29 +9,45 @@ begin
     Thesis.rdf_annotation_for_attr(key).present? ? RDF::URI(Thesis.rdf_annotation_for_attr(key).first.predicate).pname.to_s : key
   end
 
+  community_collection_headers = [
+    'Community title',
+    'Community URL',
+    'Collection title',
+    'Collection URL',
+    'Community Collection pair'
+  ]
+
+  def get_community_collection_values(collection)
+    community = collection.community
+
+    [
+      community.title,
+      Rails.application.routes.url_helpers.community_url(community.id),
+      collection.title,
+      Rails.application.routes.url_helpers.collection_url(collection.id),
+      collection.path
+    ]
+  end
+
   item_csv = CSV.open(
-    root_directory + '/report_6_item' + + '_community_collection_' + Time.now.to_formatted_s(:number) + '.csv', 'wb', write_headers: true, headers: [
-      'Community Collection pair', 'URL'
-    ] + item_headers
+    root_directory + '/report_6_item' + + '_community_collection_' + Time.now.to_formatted_s(:number) + '.csv', 'wb',
+    write_headers: true,
+    # headers: ['Community Collection pair', 'URL'] + item_headers
+    headers: community_collection_headers + ['URL'] + item_headers
   )
   thesis_csv = CSV.open(
-    root_directory + '/report_6_thesis' + + '_community_collection' + Time.now.to_formatted_s(:number) + '.csv', 'wb', write_headers: true, headers: [
-      'Community Collection pair', 'URL'
-    ] + thesis_headers
+    root_directory + '/report_6_thesis' + + '_community_collection' + Time.now.to_formatted_s(:number) + '.csv', 'wb',
+    write_headers: true,
+    # headers: ['Community Collection pair', 'URL'] + thesis_headers
+    headers: community_collection_headers + ['URL'] + thesis_headers
   )
-
-  def get_collection_url(collection)
-    # URL example: https://era.library.ualberta.ca/communities/34de6895-e488-440b-b05c-75efe26c4971/collections/67e0ecb3-05b7-4c9a-bf82-31611e2dc0ce
-    format('https://era.library.ualberta.ca/communities/%{community_id}/collections/%{collection_id}',
-           community_id: collection.community.id, collection_id: collection.id)
-  end
 
   Collection.find_each do |collection|
     collection.member_objects.each do |entity|
       if entity.instance_of?(Item)
-        item_csv << [collection.path, get_entity_url(entity)] + entity.values_at(Item.first.attributes.keys)
+        item_csv << get_community_collection_values(collection) + [get_entity_url(entity)] + entity.values_at(Item.first.attributes.keys)
       elsif entity.instance_of?(Thesis)
-        thesis_csv << [collection.path, get_entity_url(entity)] + entity.values_at(Thesis.first.attributes.keys)
+        thesis_csv << get_community_collection_values(collection) + [get_entity_url(entity)] + entity.values_at(Thesis.first.attributes.keys)
       end
     end
   end
